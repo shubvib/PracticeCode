a0e424f06c51f9b87d7be9d6e73696214ef124f6
shubvib <shubhamvibhute20@gmaill.com>
2023-01-06 11:14:19 +0530

    second commit

--- src/Components/AxiosAPI.js
+++ src/Components/AxiosAPI.js
@@ -1,19 +1,27 @@
-import axios from "axios"
+import axios from "axios";
 
 // --------------- Fetching data using axios function--------------------
 //------------------- Axios does not required data to be converted into json ---------------
 
 function AxiosAPI() {
-    const getData = async () =>{
-      const data = await axios.get("https://jsonplaceholder.typicode.com/posts?userId=1")
-      return data
-    }
-    getData().then((response)=>{
-      console.log("AxiosAPI result-------------------")
-      console.log(response.data)
-    }).catch((err)=>{
-      console.log(err)
+  const getData = async () => {
+    const data = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts?userId=1"
+    );
+    return data;
+  };
+  getData()
+    .then((response) => {
+      if(response){
+        console.log("AxiosAPI result-------------------");
+        console.log(response.data);
+      }else{
+        throw new Error("Unable to Fetch Data using Axios")
+      }
     })
-    getData()
-  }
-  export default (AxiosAPI)
+    .catch((err) => {
+      console.log(err);
+    });
+  getData();
+}
+export default AxiosAPI;
--- src/Components/FetchAPI.js
+++ src/Components/FetchAPI.js
@@ -1,22 +1,31 @@
-
-
 // --------------- Fetching data using fetch function--------------------
 //------------------- Fetch required data to be converted into json ---------------
 
 function FetchAPI() {
-  const getData = async () =>{
-    const data = await fetch("https://jsonplaceholder.typicode.com/posts?userId=1")
-    return data
-  }
-  getData().then(async (response)=>{
-    const jsonData = await response.json();  {/* converting data into json format */}
-    return jsonData
-  }).then((result)=>{
-    console.log("FetchAPI result-------------------")
-    console.log(result)
-  }).catch((err)=>{
-    console.log(err)
-  })
+  const getData = async () => {
+    const data = await fetch(
+      "https://jsonplaceholder.typicode.com/posts?userId=1"
+    );
+    return data;
+  };
   getData()
+    .then(async (response) => {
+      if(response.ok){
+        const jsonData = await response.json();
+        return jsonData;
+      }else{
+        throw new Error("Unable to fetch Data using Fetch")
+      }
+    })
+    .then((result) => {
+      console.log("FetchAPI result-------------------");
+      result.map(value =>{
+        console.log(value);
+      })
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  getData();
 }
-export default (FetchAPI)
+export default FetchAPI;
